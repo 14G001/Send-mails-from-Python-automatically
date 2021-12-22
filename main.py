41475824bb26adf3fe6c8a94d33ed04c192d24cd
Iago <90991274+14G001@users.noreply.github.com>
2021-12-22 03:42:24 -1200

    Update main.py

--- main.py
+++ main.py
@@ -6,28 +6,32 @@ import requests
 mensaje = {  }
 
 ''' Podemos saber que las claves son "name", "email" y "message"
-por el codigo HTML de la pagina como vimos anteriormente:
+por el codigo HTML de la pagina. '''
 
-No encontre el archivo que decia el enunciado en el Alumni
-"webformlab.py", por lo que lo hago con el archivo visto
-"webform.py" y reemplazo lo de enviar una inscripcion por enviar
-un mensaje. '''
+print("""Usted enviara un mensaje desde la pagina \"http://localhost:8880/form\".
+Recuerde que debera ejecutar el programa \"webform.py\" en simultaneo con este
+para poder acceder a la pagina web.\n""")
 
 mensaje['name'] = input("Nombre: ")
 mensaje['email'] = input("Email: ")
 mensaje['message'] = input("Mensaje: ")
 
-respuesta = requests.post("http://127.0.0.1:8880/form",
-	data = mensaje
-)
+try:
+	respuesta = requests.post("http://127.0.0.1:8880/form",
+		data = mensaje
+	)
+except:
+	print("\nERROR: Recuerde ejecutar el programa \"webform.py\" de manera simultanea con este mismo programa, ya que la pagina que se utiliza para enviar el mensaje (http://localhost:8880/form) es hosteada con el mismo \"webform.py\".")
+	input("\nIngrese lo que sea para salir: ")
+	quit()
+
+# print(respuesta.status_code) # 200 right.
 
 contenido_respuesta = respuesta.content.decode("utf-8")
 
-print(respuesta.status_code) # Imprime 200.
-
 if "Mensaje enviado correctamente" in contenido_respuesta:
-	print("¡Alumno inscripto correctamente!\n")
+	print("\nEl mensaje ha sido enviado correctamente.\n")
 else:
-	print("Debes ingresar todos los campos.")
+	print("\nERROR: Debes ingresar todos los campos para poder enviar el mensaje.")
 
-input("Ingrese lo que sea para salir: ")
+input("\nIngrese lo que sea para salir: ")
